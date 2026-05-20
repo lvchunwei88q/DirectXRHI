ecaece784e69709b5eb25d29e3390b1222b8d6cd
LvChunWei <213587437@qq.com>
2026-05-20 18:16:27 +0800

    优化命名

--- Engine/Source/RHI/RHI/Public/RHIDynamicLoader.h
+++ Engine/Source/RHI/RHI/Public/RHIDynamicLoader.h
@@ -4,6 +4,15 @@
 #include <memory>
 #include "RHI.h"
 
+/*
+*注意这里使用def+cmake的方式保证C++的名称修饰，否则会导致函数调用时的名称修饰与DLL导出的
+*函数调用时的名称修饰不一致，从而导致函数调用失败。
+*语法类似:
+*target_link_options(Target PRIVATE 
+*    /DEF:${CMAKE_CURRENT_SOURCE_DIR}/Private/TargetFile.def
+*)
+**/
+
 namespace RHI
 {
     // DLL 导出函数类型定义
